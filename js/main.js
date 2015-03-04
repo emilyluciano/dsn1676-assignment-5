d04899e42d841effa7818e88683924b428c3e670
emilyluciano <emilydianeluciano@hotmail.com>
2015-03-04 14:23:04 -0500

    troubles syncing the to do list

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
-var $form = $('.form');
-var $name = $('.main-input');
-var $list = $('.list');
+var $form = $('.form-box');
+var $name = $('.main-text-holder');
+var $text-box = $('.to-do-text');
+
+$form.on ('submit', function (e) {
+    e.preventDefault();
+    
+    var $li = $('<li>').html($name.val());
+    var $nameDl = $('button class="delete">X</button>');
+
+    $nameDl.on('click', function () {
+        $li.remove('li');
+    });
+    
+    $text-box.on('click', 'li', function () {
+        $(this).addClass('complete');
+    });
+    
+    $li.append($nameDl);
+    $text-box.prepend($li);
+    $name.val('');
+});
